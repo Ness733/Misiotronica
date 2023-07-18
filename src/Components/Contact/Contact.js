e0ecaa5f9c7aafd58ef5adf884d2d4e4c85dced2
Nestor Rosales <rosales.nestorr@gmail.com>
2023-07-18 14:23:44 -0300

    Contact page and sidebar position

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,21 @@
 import styles from "./Contact.module.css";
 
 export function Contact() {
-  return <h1>Contacto</h1>;
+  return (
+    <div>
+      <form className={styles.contactDiv}>
+        <h1>Contacto</h1>
+        <label for="fname">Full Name</label>
+        <input type="text" name="fullname" placeholder="Your full name." />
+        <label for="lname">Contact E-mail</label>
+        <input type="text" name="email" placeholder="Contact Email." />
+        <textarea
+          className={styles.textArea}
+          name="subject"
+          placeholder="Your message here..."
+        ></textarea>
+        <input className={styles.submitBtn} type="submit" value="Send" />
+      </form>
+    </div>
+  );
 }
