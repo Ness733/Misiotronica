2ca0dd424a2562b2fdd149a8816629e21df89770
Nestor Rosales <rosales.nestorr@gmail.com>
2023-07-20 03:58:10 -0300

    iterate list to generate cards function

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,12 +1,21 @@
 import styles from "./Card.module.css";
+// import listaArticulos from "../Sales/listaArticulos";
 
-function Card(props) {
+function Card({ objeto }) {
   return (
-    <div className={styles.cardTile}>
-      <h3>{props.cardTitle}</h3>
-      <img src={props.imgSrc} alt=""></img>
-      <p>{props.cardText}</p>
-      <button className={styles.consultarBtn}>CONSULTAR</button>
+    <div className={styles.list}>
+      {objeto.map((item) => {
+        return (
+          <div key={item.nombre} className={styles.cardTile}>
+            <li>
+              <h3>{item.nombre}</h3>
+              <img src={item.imagen} alt=""></img>
+              {/* <p>{props.cardText}</p> */}
+              <button className={styles.consultarBtn}>CONSULTAR</button>
+            </li>
+          </div>
+        );
+      })}
     </div>
   );
 }
