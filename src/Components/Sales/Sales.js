ec1cdfa21ca58befda285d9b8586ffe1ca086056
Nestor Rosales <rosales.nestorr@gmail.com>
2023-07-20 04:01:03 -0300

    pagination implementation

--- src/Components/Sales/Sales.js
+++ src/Components/Sales/Sales.js
@@ -1,20 +1,36 @@
 import styles from "./Sales.module.css";
 import Card from "../Card/Card";
-import logo from "../../Imagenes/misiotronica_logo.png";
+import listaArticulos from "./listaArticulos";
+import { useState } from "react";
+import { Pagination } from "../Pagination/Pagination";
 
-function Main() {
+function Ventas() {
+  const [currentPage, setCurrentPage] = useState([1]);
+  const [itemsPerPage] = useState(6);
+
+  // current item
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = listaArticulos.slice(indexOfFirstItem, indexOfLastItem);
+
+  // change page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <section className={styles.hero}>
-      <ul className={styles.list}>
-        <Card cardTitle="Item 1" imgSrc={logo} cardText="Descripción" />
-        <Card cardTitle="Item 2" imgSrc={logo} cardText="Descripción" />
-        <Card cardTitle="Item 3" imgSrc={logo} cardText="Descripción" />
-        <Card cardTitle="Item 4" imgSrc={logo} cardText="Descripción" />
-        <Card cardTitle="Item 5" imgSrc={logo} cardText="Descripción" />
-        <Card cardTitle="Item 6" imgSrc={logo} cardText="Descripción" />
+      <h1>Ventas</h1>
+      <ul>
+        <Card objeto={currentItems} />
       </ul>
+      <Pagination
+        itemsPerPage={itemsPerPage}
+        totalItems={listaArticulos.length}
+        paginate={paginate}
+      />
     </section>
   );
 }
 
-export default Main;
+export default Ventas;
