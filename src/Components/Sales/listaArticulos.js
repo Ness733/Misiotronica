b01ee415d871ad79236941991523871af9545a69
Nestor Rosales <rosales.nestorr@gmail.com>
2023-07-20 04:00:43 -0300

    array of products for card generation

---
+++ src/Components/Sales/listaArticulos.js
@@ -0,0 +1,119 @@
+const listaArticulos = [
+  {
+    nombre: "Estaño Eximetal 1mm 60/40 Soldadura Electronica Rollo",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_665302-MLA45836620702_052021-O.webp",
+  },
+  {
+    nombre: "Soldador Estaño 40w Lápiz Cautín Punta Eléctrico Profesional",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_629539-MLU70356568839_072023-V.webp",
+  },
+  {
+    nombre: "Display Lcd 16x2 1602 Azul C/ I2c Serie Arduino Pic Arm",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_839638-MLA31351570466_072019-O.webp",
+  },
+  {
+    nombre: "Display Led 1 Digito 7 Segmentos Arduino Anodo Comun Rojo",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_994036-MLA52961094234_122022-O.webp",
+  },
+  {
+    nombre: "Cable Rca Miniplug Audio Auxiliar Macho 3,5 Mm 1.5 Mts",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_866078-MLA52264754219_112022-O.webp",
+  },
+  {
+    nombre: "Cable Auxiliar Mini Plug 3.5 Mm. Macho 1,5 Metros",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_620103-MLA31015579271_062019-O.webp",
+  },
+  {
+    nombre: "Led Cob 50w Blanco Frio 220v C/bornera 6000/6500k",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_956914-MLA42755669544_072020-O.webp",
+  },
+  {
+    nombre: "Fuente Switching 12v 10a Regulada Proteccion Tira Led Cctv",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_686531-MLA49116804818_022022-V.webp",
+  },
+  {
+    nombre: "Voltimetro Amperimetro Digital Led Dc 0v A 100v 10a",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_630594-MLA46247708726_062021-V.webp",
+  },
+  {
+    nombre: "Placa Arduino Nano Usb C Con Pines Soldados -atmega328 Atmel",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_882638-MLA69408087797_052023-V.webp",
+  },
+  {
+    nombre: "Modulo Conversor Rs485 Ttl Max485 Transceiver Arduino Hobb",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_949013-MLA52962654710_122022-O.webp",
+  },
+  {
+    nombre: "Cargador Bateria Litio 5v Tp4056 Con Proteccion",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_919426-MLA51036643665_082022-O.webp",
+  },
+  {
+    nombre: "Encoder Rotativo Ky-040 Módulo - Arduino",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_725138-MLA41148508181_032020-O.webp",
+  },
+  {
+    nombre: "Teclado Matricial 4x4 Autoadhesivo Electronica Arduino",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_750984-MLA52962744554_122022-O.webp",
+  },
+  {
+    nombre: "Módulo Led Rgb Smd 5050pwm 3 Colores Ky-009 - Arduino",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_655618-MLA42538188902_072020-O.webp",
+  },
+  {
+    nombre: "Hc-sr04 Módulo Sensor Ultrasónico - Arduino",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_686957-MLA43844630619_102020-O.webp",
+  },
+  {
+    nombre: "Módulo Sensor Detector/seguidor De Línea Ky-033 - Arduino",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_775516-MLA42644278316_072020-O.webp",
+  },
+  {
+    nombre: "Módulo Sensor Infrarrojo Detector De Obstáculos Fc51 Arduino",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_865333-MLA44877539081_022021-O.webp",
+  },
+  {
+    nombre: "Servo Motor Tower Pro Torque 1.3 Kg/cm Sg90 9g Arduino",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_876345-MLA45077714109_032021-O.webp",
+  },
+  {
+    nombre: "Mini Bomba Sumergible De Agua Dc3-5v 80-100l/h - Arduino",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_773431-MLA46683328868_072021-O.webp",
+  },
+  {
+    nombre: "Módulo Display 8 Digitos 7 Segmentos C/max7219 - Arduino",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_936817-MLA43693878996_102020-O.webp",
+  },
+  {
+    nombre: "Modulo Amplificador De Audio Tda 7293 Mono 100w Rms Tda7293",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_933367-MLA46493946458_062021-O.webp",
+  },
+  {
+    nombre: "Modulo Amplificador De Audio Estereo 2 X 100w Clase D Tpa3116d2",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_722787-MLA51846141799_102022-O.webp",
+  },
+];
+
+export default listaArticulos;
