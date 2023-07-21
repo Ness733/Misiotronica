2e539872c2af2f52e76e691ef4d7d33b387e8c1e
Nestor Rosales <rosales.nestorr@gmail.com>
2023-07-21 07:39:12 -0300

    receive value from search box and reflect changes

--- src/Components/Sales/Sales.js
+++ src/Components/Sales/Sales.js
@@ -3,8 +3,10 @@ import Card from "../Card/Card";
 import listaArticulos from "./listaArticulos";
 import { useState } from "react";
 import { Pagination } from "../Pagination/Pagination";
+import { useSelector } from "react-redux";
 
 function Ventas() {
+  const currentFilter = useSelector((state) => state.searchContent);
   const [currentPage, setCurrentPage] = useState([1]);
   const [itemsPerPage] = useState(6);
 
@@ -12,25 +14,41 @@ function Ventas() {
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = listaArticulos.slice(indexOfFirstItem, indexOfLastItem);
+  console.log(currentFilter);
 
+  const newItems = listaArticulos.filter((item) =>
+    item.nombre.includes(currentFilter)
+  );
+  console.log(newItems);
   // change page
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
+  const switchList = () =>
+    newItems.length < listaArticulos.length
+      ? newItems.length
+      : listaArticulos.length;
+
+  const renderItems = () =>
+    newItems.length < listaArticulos.length ? newItems : currentItems;
+
   return (
-    <section className={styles.hero}>
-      <h1>Ventas</h1>
-      <ul>
-        <Card objeto={currentItems} />
-      </ul>
-      <Pagination
-        itemsPerPage={itemsPerPage}
-        totalItems={listaArticulos.length}
-        paginate={paginate}
-        currentPage={currentPage}
-      />
-    </section>
+    console.log(newItems.length, listaArticulos.length),
+    (
+      <section className={styles.hero}>
+        <h1>Ventas</h1>
+        <ul>
+          <Card objeto={renderItems()} />
+        </ul>
+        <Pagination
+          itemsPerPage={itemsPerPage}
+          totalItems={switchList()}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
+      </section>
+    )
   );
 }
 
