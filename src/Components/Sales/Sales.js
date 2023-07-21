170ed72e68c6cd741beb70dcd132a847a74536c6
Nestor Rosales <rosales.nestorr@gmail.com>
2023-07-21 20:34:13 -0300

    add accents to products on rendering

--- src/Components/Sales/Sales.js
+++ src/Components/Sales/Sales.js
@@ -3,53 +3,100 @@ import Card from "../Card/Card";
 import listaArticulos from "./listaArticulos";
 import { useState } from "react";
 import { Pagination } from "../Pagination/Pagination";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { searchText } from "../Redux/store";
+// import { useNavigate } from "react-router";
 
-function Ventas() {
+function Sales() {
   const currentFilter = useSelector((state) => state.searchContent);
   const [currentPage, setCurrentPage] = useState([1]);
   const [itemsPerPage] = useState(6);
+  const dispatch = useDispatch();
+  // const navigate = useNavigate();
 
   // current item
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = listaArticulos.slice(indexOfFirstItem, indexOfLastItem);
-  console.log(currentFilter);
+  // console.log(currentFilter);
 
-  const newItems = listaArticulos.filter((item) =>
-    item.nombre.includes(currentFilter)
+  // items list without special character: ´
+  const listaArticulosString = listaArticulos.map((element) =>
+    element.nombre.replace("ó", "o")
   );
-  console.log(newItems);
+
+  // const [allItems, resetItems] = useState(currentItems);
+
+  const x = listaArticulosString.filter((word) => word.includes(currentFilter));
+  const y = x.map((item) => item.replace("Modulo", "Módulo"));
+  // console.log(y);
+
+  // console.log(listaArticulosString);
+  const newItems = () => {
+    let c = [];
+    for (let i = 0; i < y.length; i++) {
+      // console.log(listaArticulos.filter((item) => item.nombre.includes(y[i])));
+      c.push(...listaArticulos.filter((item) => item.nombre.includes(y[i])));
+    }
+    return c;
+  };
+
+  // listaArticulos.filter((item) => item.nombre.includes(currentFilter));
+
   // change page
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
   const switchList = () =>
-    newItems.length < listaArticulos.length
-      ? newItems.length
-      : listaArticulos.length;
+    y.length < listaArticulos.length ? y.length : listaArticulos.length;
 
-  const renderItems = () =>
-    newItems.length < listaArticulos.length ? newItems : currentItems;
+  const renderItems = () => (currentFilter === " " ? currentItems : newItems());
+  // y.length < listaArticulos.length ? newItems() : currentItems;
 
   return (
-    console.log(newItems.length, listaArticulos.length),
-    (
-      <section className={styles.hero}>
-        <h1>Ventas</h1>
-        <ul>
-          <Card objeto={renderItems()} />
-        </ul>
-        <Pagination
-          itemsPerPage={itemsPerPage}
-          totalItems={switchList()}
-          paginate={paginate}
-          currentPage={currentPage}
-        />
-      </section>
-    )
+    <section className={styles.hero}>
+      <h1>Ventas</h1>
+      <h2>Consultar por disponibilidad de stock</h2>
+      {y.length < listaArticulos.length ? (
+        <button
+          className={styles.returnBtn}
+          onClick={() => {
+            dispatch(searchText(" "));
+            renderItems();
+          }}
+        >
+          VOLVER
+        </button>
+      ) : (
+        ""
+      )}
+
+      <ul>
+        <Card objeto={renderItems()} />
+      </ul>
+      <Pagination
+        itemsPerPage={itemsPerPage}
+        totalItems={switchList()}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
+      {y.length < listaArticulos.length ? (
+        <button
+          className={styles.returnBtn}
+          onClick={() => {
+            dispatch(searchText(" "));
+            renderItems();
+            document.getElementById("Navbar").scrollIntoView("smooth");
+          }}
+        >
+          VOLVER
+        </button>
+      ) : (
+        ""
+      )}
+    </section>
   );
 }
 
-export default Ventas;
+export default Sales;
