86ec15e38cbfb57068944921f2ad44a3d68f7012
Nestor Rosales <rosales.nestorr@gmail.com>
2023-07-26 19:56:12 -0300

    remove pages on displays < 1000px

--- src/Components/Sales/Sales.js
+++ src/Components/Sales/Sales.js
@@ -1,7 +1,7 @@
 import styles from "./Sales.module.css";
 import Card from "../Card/Card";
 import { itemsList } from "./listaArticulos";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Pagination } from "../Pagination/Pagination";
 import { useSelector, useDispatch } from "react-redux";
 import { searchText } from "../Redux/store";
@@ -12,19 +12,41 @@ function Sales() {
   const dispatch = useDispatch();
   // Items por página y página inicial
   const [currentPage, setCurrentPage] = useState([1]);
-  const [itemsPerPage] = useState(6);
+  const [itemsPerPage, setItemsPerPage] = useState(6);
   // Obtener items según itemsPerPage
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = itemsList.slice(indexOfFirstItem, indexOfLastItem);
+  // const screenWidth = window.innerWidth;
+  // console.log(screenWidth);
 
   // Filtrar items según el input de la barra de búsqueda
   const filteredItems = itemsList.filter((word) =>
     word.nombre.includes(currentFilter)
   );
+  // Tomar dimensiones de la pantalla
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  let width;
+  const handleResize = () => {
+    let width = window.innerWidth;
 
+    return width;
+  };
+  handleResize();
+
+  // Renderizar todos los articulos en mobile
+  useEffect(() => {
+    // eslint-disable-next-line
+    width = handleResize();
+    width < 1000 ? setItemsPerPage(itemsList.length) : setItemsPerPage(6);
+  });
   // Cambiar la página actual
-  const paginate = (pageNumber) => {
+  const paginate = (pageNumber = 1) => {
     setCurrentPage(pageNumber);
   };
 
