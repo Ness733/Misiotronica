614b2251364ec70cfd56b368bbf03df9a2ab3433
Nestor Rosales <rosales.nestorr@gmail.com>
2023-07-28 09:52:18 -0300

    implementation to add products to cart

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,7 +1,23 @@
 import styles from "./Card.module.css";
-// import listaArticulos from "../Sales/listaArticulos";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../Redux/store";
 
 function Card({ objeto }) {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.list);
+  console.log(products);
+  const addProductToCart = (product) => {
+    const newProduct = {
+      nombre: product.nombre,
+      imagen: product.imagen,
+      id: product.id,
+      precio: product.precio,
+      cantidad: 1,
+    };
+
+    dispatch(addToCart(newProduct));
+  };
+
   return (
     <div className={styles.list}>
       {objeto.map((item) => {
@@ -9,9 +25,20 @@ function Card({ objeto }) {
           <div key={item.nombre} className={styles.cardTile}>
             <li>
               <h3>{item.nombre}</h3>
-              <img src={item.imagen} alt=""></img>
-              {/* <p>{props.cardText}</p> */}
-              <button className={styles.consultarBtn}>CONSULTAR</button>
+              <img src={item.imagen} alt={item.nombre}></img>
+              <p className={styles.Price}>{`$${item.precio}`}</p>
+              <button
+                onClick={() => {
+                  if (products.some((product) => product.id === item.id))
+                    return;
+                  else {
+                    addProductToCart(item);
+                  }
+                }}
+                className={styles.consultarBtn}
+              >
+                Añadir al carrito
+              </button>
             </li>
           </div>
         );
