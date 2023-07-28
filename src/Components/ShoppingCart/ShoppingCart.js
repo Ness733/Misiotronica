71a4efafca43aaee89942b01719bdf272bda9a50
Nestor Rosales <rosales.nestorr@gmail.com>
2023-07-28 09:48:57 -0300

    shop cart component

---
+++ src/Components/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,68 @@
+import styles from "./ShoppingCart.module.css";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { FaTrash } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { RemoveFromCart, changeQty } from "../Redux/store";
+
+const ShoppingCart = function ({ visibility, onClose, onQuantityChange }) {
+  const products = useSelector((state) => state.list);
+  const dispatch = useDispatch();
+  return (
+    <div
+      className={styles.Modal}
+      style={{ display: visibility ? "block" : "none" }}
+    >
+      <div className={styles.ShoppingCart}>
+        <div className={styles.Header}>
+          <h2>Productos</h2>
+          <button className={styles.BtnClose} onClick={onClose}>
+            <AiFillCloseCircle size={70} />
+          </button>
+        </div>
+        <div className={styles.CartProducts}>
+          {products.length === 0 && (
+            <span className={styles.EmptyText}>El carrito está vacio.</span>
+          )}
+          {products.map((product) => (
+            <div className={styles.CartProduct} key={product.id}>
+              <img src={product.imagen} alt={product.nombre} />
+              <div className={styles.ProductInfo}>
+                <h3>{product.nombre}</h3>
+                <span className={styles.ProductPrice}>
+                  ${product.precio * product.cantidad}
+                </span>
+              </div>
+              <select
+                className={styles.Count}
+                value={product.count}
+                onChange={(event) => {
+                  dispatch(changeQty(product.id, Number(event.target.value)));
+                }}
+              >
+                {[...Array(10).keys()].map((number) => {
+                  const num = number + 1;
+                  return (
+                    <option value={num} key={num}>
+                      {num}
+                    </option>
+                  );
+                })}
+              </select>
+              <button
+                className={styles.BtnRemove}
+                onClick={() => dispatch(RemoveFromCart(product))}
+              >
+                <FaTrash size={20} />
+              </button>
+            </div>
+          ))}
+          {products.length > 0 && (
+            <button className={styles.Checkout}>Ir a pagar</button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShoppingCart;
