279f73c83029e6bd1c41fd844b4c89eb0c94c4d8
Nestor Rosales <rosales.nestorr@gmail.com>
2023-07-28 20:57:31 -0300

    added img and replaced lorem

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,14 +1,22 @@
 import styles from "./About.module.css";
+import frente from "../../Imagenes/misiotronica_local.jpg";
 
 export function About() {
   return (
     <div className={styles.About}>
       <h1>Sobre Nosotros</h1>
+      <img src={frente} alt="Frente Casa Central" />
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sed,
-        adipisci nihil nemo consequatur esse ducimus quaerat totam dolorem
-        incidunt itaque voluptates ipsum officiis labore impedit quisquam,
-        soluta, deserunt nisi.
+        Misiotrónica nace como una respuesta sencilla a una pregunta milenaria.
+        ¿Dónde puedo conseguir esto? Desde nuestros inicios en 2009 nos hemos
+        puesto como objetivo diario el poder facilitar a nuestros clientes los
+        componentes necesarios para que puedan realizar los suyos. <br /> <br />
+        Nuestra ámplia carta de proveedores internacionales nos permite disponer
+        siempre del componente preciso para el trabajo, y no lo que más se le
+        parezca.
+        <br /> <br />
+        Los invitamos a recorrer nuestro sitio y visitar nuestra casa central en
+        Av. Zapiola 2922, Posadas, Misiones.
       </p>
     </div>
   );
