3f8ebdb10265b26d46fe1c3b404630da13bee343
Nestor Rosales <rosales.nestorr@gmail.com>
2023-07-28 09:51:18 -0300

    added cart funcionalities/reducer

--- src/Components/Redux/store.js
+++ src/Components/Redux/store.js
@@ -1,23 +1,86 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-const searchInput = "Search";
-
-export function searchText(value) {
+export const searchText = (value) => {
   return {
-    type: searchInput,
+    type: "Search",
     value: value,
   };
-}
+};
+
+export const addToCart = (object) => {
+  return {
+    type: "Add Product",
+    payload: {
+      nombre: object.nombre,
+      imagen: object.imagen,
+      id: object.id,
+      precio: object.precio,
+      cantidad: object.cantidad,
+    },
+  };
+};
+
+export const changeQty = (objectId, cantidad) => {
+  return {
+    type: "Change Quantity",
+    payload: {
+      id: objectId,
+      cantidad,
+    },
+  };
+};
+
+export const RemoveFromCart = (object) => {
+  return {
+    type: "Remove Product",
+    payload: object.id,
+  };
+};
+
+const removeProduct = (Array, id) => {
+  return Array.filter((obj) => obj.id !== id);
+};
 
 const initialState = {
+  list: [],
   searchContent: "",
 };
 
-const reducer = (state = initialState, action) => {
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case searchInput:
+    case "Search":
       return {
+        ...state,
         searchContent: action.value,
+      };
+    case "Add Product":
+      return {
+        ...state,
+        list: [...state.list, action.payload],
+      };
+    case "Remove Product":
+      const newList = removeProduct(state.list, action.payload);
+      return {
+        ...state,
+        list: newList,
+      };
+    case "Change Quantity":
+      const { id, cantidad } = action.payload;
+
+      const changeCantidad = (id, cantidad) => {
+        const temp = [...state.list];
+
+        const newArray = temp.map((item) => {
+          if (item.id === id) {
+            return { ...item, cantidad: cantidad };
+          }
+          return item;
+        });
+        return newArray;
+      };
+      return {
+        ...state,
+        list: changeCantidad(id, cantidad),
       };
     default:
       return state;
